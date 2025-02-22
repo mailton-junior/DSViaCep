af19d43da78ca625d1b81de1b6b4a89efa4a7760
Mailton Junior <mailtonjr7@gmail.com>
2025-02-22 20:20:34 -0300

    add events
    - add event clear
    - handle inputNumber message

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,4 +1,4 @@
-import Address from "../models/address";
+import * as Address from "../models/address.js";
 
 function State() {
   this.Address = newAddress;
@@ -26,8 +26,39 @@ export function init() {
   state.btnSave = document.forms.newAddress.btnSave;
   state.btnClear = document.forms.newAddress.btnClear;
 
-  state.errorCep = document.querySelector('[data-error="cep"]');
-  state.errorNumber = document.querySelector('[data-error="number"]');
+  state.errorCep = document.querySelector(`[data-error="cep"]`);
+  state.errorNumber = document.querySelector(`[data-error="number"]`);
 
-  console.log(state);
+  state.inputNumber.addEventListener("change", handleInputNumberChange);
+  state.btnClear.addEventListener("click", handlebtnClearClick);
+}
+
+function handleInputNumberChange(event) {
+  if (event.target.value == "") {
+    setFormError("number", "Campo requerido");
+  } else {
+    setFormError("number", "");
+  }
+}
+
+function handlebtnClearClick(event) {
+  event.preventDefault();
+  clearForm();
+}
+
+function clearForm() {
+  state.inputCep.value = "";
+  state.inputCity.value = "";
+  state.inputNumber.value = "";
+  state.inputStreet.value = "";
+
+  setFormError("cep", "");
+  setFormError("number", "");
+
+  state.inputCep.focus();
+}
+
+function setFormError(key, value) {
+  const element = document.querySelector(`[data-error="${key}"]`);
+  element.innerHTML = value;
 }
